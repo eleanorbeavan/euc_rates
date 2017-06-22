52e31710088b31220dfdc829f9d2ad81ace00fc9
Eleanor Beavan <eleanorlbeavan@gmail.com>
2017-06-22 15:29:15 +1000

    Update function_to_get_models_200_trees.R

--- function_to_get_models_200_trees.R
+++ function_to_get_models_200_trees.R
@@ -63,22 +63,98 @@ prune.tree = function(tree, x) {
 
 # loop to perform "prune.tree" 200 times and create list of 200 pruned trees
 output = list()
-times = 2
+times = 200
 for (i in 1:times) {
     newtree = prune.tree(tree)
     output = c(output, list(newtree))
 }
 
 # apply chronos to file with 200 pruned trees to get rates for all 200 trees
-
-# create dataframe with node ages
-makeChronosCalib(tree, node="root", age.min = 1, age.max = 1, interactive = F)
-
 # create chronogram for all 200 pruned trees
 make.chronogram = function(tree) {
     c = chronos(tree, lambda = 1, calibration = makeChronosCalib(tree))
+    print(c)
     return(c)
 }
 
-chronograms = lapply(pruned.trees, make.chronogram)
+# extract rates from 200 trees and create dataframe
+extract.rates = function(c) {
+    # create dataframe with all nodes in tree
+    f = data.frame(c$edge)
+    # create a new column with the rate for each branch
+    f$rate = attr(c, "rate")
+    # keep only terminal branches
+    e = f[f$X2<=length(c$tip.label),]
+    # add species names
+    e$accepted_name = c$tip.label
 
+    return(e)
+}
+
+# creates function to 
+    # 1. make 200 chronograms
+    # 2. extract rates and create 200 dataframes
+get.rates = function(tree, c) {
+    chronograms = mclapply(pruned.trees, make.chronogram, mc.cores = 10)
+    rates = mclapply(chronograms, extract.rates, mc.cores = 10)
+    return(rates)
+}
+
+rates = mclapply(pruned.trees[1:3], get.rates, mc.cores = 10)
+
+# merge rate data with LHT data
+merge.data = function(rate, trait) {
+    merged = merge(rate_data, lhtdata, by = "accepted_name")
+    return(merged)
+}
+
+LHD = mclapply(rates, merge.data, mc.cores = 10)
+
+# perform PGLS on 200 dataframes for 5 models
+    # rate ~ genome size
+    # rate ~ height
+    # rate ~ genome size
+    # rate ~ SLA
+    # rate ~ all traits
+
+# names in tree must be same as names in dataframe
+# check which species are not in dataframe and drop tips from tree
+
+# perform PGLS on 200 dataframes for 5 models
+    # rate ~ genome size
+    # rate ~ height
+    # rate ~ genome size
+    # rate ~ SLA
+    # rate ~ all traits
+
+# names in tree must be same as names in dataframe
+# check which species are not in dataframe and drop tips from tree
+tips.to.drop = list()
+for (i in 1:200) {
+    treetips = chronograms[[i]]$tip.label
+    treetips = na.omit(treetips)
+    datatips = as.character(traits$accepted_name)
+    tips = setdiff(treetips, datatips)
+    tips = append(tips, NA)
+    tips.to.drop = c(tips.to.drop, list(tips))
+}
+
+mod.1 = list()
+results.mod.1 = data.frame(tree_number = "", p_value = "", r_squared = "", StringAsFactors = FALSE)
+
+for (i in 1:200) {
+    tree = chronograms[[i]]
+    
+    # drop tips from the tree that aren't in the dataset
+    tree = drop.tip(tree, tips.to.drop[[i]])
+    
+    # PGLS using caper
+    compdata1 = comparative.data(data = traits, phy = tree, names.col = "accepted_name", vcv = TRUE)
+    model1 = pgls(log(rate)~log(mean_gs), compdata1, delta = "ML", lambda = 1.0, kappa = 1.0)
+    mod1 = summary(model1)
+    mod.1 = c(mod.1, list(model1)
+              
+    results.mod.1 = rbind(results.mod.1, c("i", mod1$coefficients[??], mod1$r.squared) ###### CHECK THIS ######             
+ }     
+              
+   
