4346d0c1bff3e995721459fc6736edd5ba42fc32
Eleanor Beavan <eleanorlbeavan@gmail.com>
2018-02-08 11:50:47 +1100

    Update ML_wholecp.R

--- ML_wholecp.R
+++ ML_wholecp.R
@@ -20,50 +20,10 @@ height = lhtdata[complete.cases(lhtdata$mean_height),]
 # ONLY USE when you are interested in average trait data
 lhtdata = read.csv("your path to average trait data")
 
-# three functions to 
-# 1. find shortest branch length
-# 2. remove shortest branch 
-# 3. drop all branches <5 substitutions in alignment
+# use prune.tree() function. This is found in the 'prune_short_branches_fn' file
 
-# function to find the shortest branch length in the tree
-shortest.tip = function(tree) {
-    n.tips = length(tree$tip.label)
-    descendant.nodes = tree$edge[,2]
-    terminal.edges = tree$edge.length[descendant.nodes<=n.tips]
-    return(min(terminal.edges))
-}
-
-# function to remove shortest bl
-prune.shortest.branch = function(tree) {
-    # take the edge length of those <= the number of tip labels (terminal nodes)
-    # vector with terminal branch lengths
-    n.tips = length(tree$tip.label)
-    descendant.nodes = tree$edge[,2]
-    terminal.edges = tree$edge.length[descendant.nodes<=n.tips]
-    # round terminal.edges to 5 decimal places
-    terminal.edges = round(terminal.edges, digits = 5)
-    # keep random shortest tips
-    shortest.tips = which(terminal.edges == min(terminal.edges))
-    # the loop is needed because if there is only 1 value for shortest tip, the function will return any number from 1:x
-    if(length(shortest.tips)>1){ 
-        shortest.tip = sample(shortest.tips, 1)
-    }else{
-        shortest.tip = shortest.tips
-    }
-    
-    new.tree = drop.tip(tree, shortest.tip) 
-    return(new.tree)
-}
-
-# use a loop to remove all branches with <5 substitutions
-x = (5/139598)
-prune.tree = function(tree, x) {
-    while (shortest.tip(tree) < x) {
-        tree = prune.shortest.branch(tree)
-    }
-    return(tree)
-}
-
+# prune branches with <5 substitutions
+x = 5/(length of alignment)
 pruned.tree = prune.tree(tree, x)
 
 #------------------------------------------------------------
