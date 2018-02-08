37b763072e4852a8b4d555b912b66840b5a72b32
Eleanor Beavan <eleanorlbeavan@gmail.com>
2018-02-08 11:52:24 +1100

    Update coevol_setup.R

--- coevol_setup.R
+++ coevol_setup.R
@@ -22,46 +22,7 @@ tree$tip.label=seqdata$coevol[match(tree$tip.label, seqdata$alignment_name)]
 # remove NAs
 cptree = drop.tip(cptree, c(3, 41, 47, 115, 116, 118, 119, 162, 164, 172, 190, 192, 197, 198, 216, 299, 376, 401, 438, 479, 481, 502, 504, 520, 521, 547, 553, 557, 597, 645, 646, 650))
 
-#-------------------------------------------------------------------------------
-## FUNCTIONS TO PRUNE SHORTEST BRANCH
-# function to find the shortest branch length in the tree
-shortest.tip = function(tree) {
-    n.tips = length(tree$tip.label)
-    descendant.nodes = tree$edge[,2]
-    terminal.edges = tree$edge.length[descendant.nodes<=n.tips]
-    return(min(terminal.edges))
-}
-
-# function to remove shortest bl
-prune.shortest.branch = function(tree) {
-    # take the edge length of those <= the number of tip labels (terminal nodes)
-    # vector with terminal branch lengths
-    n.tips = length(tree$tip.label)
-    descendant.nodes = tree$edge[,2]
-    terminal.edges = tree$edge.length[descendant.nodes<=n.tips]
-    # round terminal.edges to 5 decimal places
-    terminal.edges = round(terminal.edges, digits = 5)
-    # keep random shortest tips
-    shortest.tips = which(terminal.edges == min(terminal.edges))
-    # the loop is needed because if there is only 1 value for shortest tip, the function will return any number from 1:x
-    if(length(shortest.tips)>1){ 
-        shortest.tip = sample(shortest.tips, 1)
-    }else{
-        shortest.tip = shortest.tips
-    }
-    
-    new.tree = drop.tip(tree, shortest.tip) 
-    return(new.tree)
-}
-
-# use a loop to remove all branches <5 substitutions
-prune.tree = function(tree, x) {
-    while (shortest.tip(tree) < x) {
-        tree = prune.shortest.branch(tree)
-    }
-    return(tree)
-}
-#-------------------------------------------------------------------------------
+# use prune.tree() function. This is found in the 'prune_short_branches_fn' file
 
 # TREE
 # drop tips with less than 5 substitutions
