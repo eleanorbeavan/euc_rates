610e38374a177e2062dbc74d6a0338fa2bc8a384
Eleanor Beavan <eleanorlbeavan@gmail.com>
2018-02-08 15:22:21 +1100

    Update commandline.sh

--- COEVOL/commandline.sh
+++ COEVOL/commandline.sh
@@ -1,1 +1,5 @@
-./coevol -d alignment_100 -t nuc_tree_100_sp.tre -c traits_100 -f nuc_100
+# without fossils
+./coevol -d "alignment_file" -t "tree_file" -c "trait_file" -bd -f "output_file_name"
+
+# with fossils
+./coevol -d "alignment_file" -t "tree_file" -c "trait_file" -cal "fossil_file" "mean" "standard deviation" -bd -f "output_file_name"
